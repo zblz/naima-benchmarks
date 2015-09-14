08036268c505ceb952c541fa3b6bbb931aed537e
Víctor Zabalza <vzabalza@gmail.com>
2015-09-14 16:15:45 +0100

    new structure

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -25,6 +25,17 @@ def IC(pars,data):
     ic = naima.models.InverseCompton(ecpl)
     return ic.sed(data)
 
+ecpl = naima.models.ExponentialCutoffPowerLaw(1e35 * u.Unit('1/eV'),
+        1*u.TeV, 2.3, 50*u.TeV)
+ic = naima.models.InverseCompton(ecpl)
+
+def IC_2(pars,data):
+    ecpl.amplitude = pars[0] / u.eV
+    ecpl.alpha = pars[1]
+    ecpl.e_cutoff = pars[2]*u.TeV
+    return ic.flux(data)
+
+
 def lnprior(pars):
     return 0
 
@@ -36,19 +47,99 @@ import os
 ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
-class TimeSuite:
+class TimeECPL:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
     def setup(self):
+        self.energy_g = np.logspace(-3, 3, 500) * u.TeV
+
+        self.ecpl = naima.models.ExponentialCutoffPowerLaw(1e36/u.eV,
+                1*u.TeV, 2.7, 50*u.TeV)
+        self.ecpl._memoize = True
+        _ = self.ecpl(self.energy_g)
+
+    def time_ECPL_1start(self):
+        ecpl = naima.models.ExponentialCutoffPowerLaw(1e36/u.eV,
+                1*u.TeV, 2.7, 50*u.TeV)
+
+    def time_ECPL_2memoize(self):
+        self.ecpl._memoize = True
+        pd = self.ecpl(self.energy_g)
+
+    def time_ECPL_3nomemo(self):
+        self.ecpl._memoize = False
+        pd = self.ecpl(self.energy_g)
+
+
+class TimeRadIC:
+    def setup(self):
         self.pdist = naima.models.ExponentialCutoffPowerLaw(1e36/u.eV,
                 1*u.TeV, 2.7, 50*u.TeV)
         self.energy_g = np.logspace(-3, 3, 500) * u.TeV
-        self.energy_x = np.logspace(-1, 6, 500) * u.eV
-        self.ecpl = naima.models.ExponentialCutoffPowerLaw(1e36/u.eV,
-                1*u.TeV, 2.7, 50*u.TeV)
 
+        self.IC = naima.models.InverseCompton(self.pdist,
+                seed_photon_fields=['CMB','FIR'])
+        _ = self.IC.sed(self.energy_g)
+
+
+    def time_IC_1start(self):
+        IC = naima.models.InverseCompton(self.pdist,
+                seed_photon_fields=['CMB', 'FIR',
+                    ['star', 20000*u.K, 1*u.erg/u.cm**3, 90*u.deg],
+                    ['NIR', 200*u.K, 1*u.eV/u.cm**3],
+                    ])
+
+    def time_IC_2memo(self):
+        _ = self.IC.flux(self.energy_g)
+
+    def time_IC_3nomemo(self):
+        self.IC._memoize = False
+        _ = self.IC.flux(self.energy_g)
+
+class TimeRadPionDecay:
+    def setup(self):
+        self.pdist = naima.models.ExponentialCutoffPowerLaw(1e36/u.eV,
+                1*u.TeV, 2.7, 50*u.TeV)
+        self.energy_g = np.logspace(-3, 3, 500) * u.TeV
+
+        self.PP = naima.models.PionDecay(self.pdist)
+        _ = self.PP.sed(self.energy_g)
+
+    def time_PionDecay_1start(self):
+        PP = naima.models.PionDecay(self.pdist)
+
+    def time_PionDecay_2memo(self):
+        self.PP._memoize = True
+        _ = self.PP.flux(self.energy_g)
+
+    def time_PionDecay_3nomemo(self):
+        self.PP._memoize = False
+        _ = self.PP.flux(self.energy_g)
+
+    def time_PionDecay_4loadLUT(self):
+        self.PP._memoize = False
+        self.PP.diffsigma.fname = ''
+        _ = self.PP.flux(self.energy_g)
+
+class TimeRadSy:
+    def setup(self):
+        self.pdist = naima.models.ExponentialCutoffPowerLaw(1e36/u.eV,
+                1*u.TeV, 2.7, 50*u.TeV)
+        self.energy_x = np.logspace(-1, 6, 500) * u.eV
+        self.Sy = naima.models.Synchrotron(self.pdist, B=1*u.mG)
+        self.Sy._memoize = False
+
+    def time_Sy_1start(self):
+        Sy = naima.models.Synchrotron(self.pdist, B=1*u.mG)
+
+    def time_Sy_2run(self):
+        _ = self.Sy.flux(self.energy_x)
+
+
+class TimePrefit:
+    def setup(self):
         self.data = ascii.read(os.path.join(ROOT,'RXJ1713_HESS_2007.dat'))
 
     def time_prefit_ecpl(self):
@@ -61,38 +152,8 @@ class TimeSuite:
                 labels=labels, model=IC, prior=lnprior, nwalkers=10, nburn=0,
                 prefit=True)
 
-    def time_ECPL(self):
-        self.ecpl._memoize = False
-        for i in range(10):
-            pd = self.ecpl(self.energy_g)
+    def time_prefit_ic2(self):
+        sampler, pos = naima.get_sampler(data_table=self.data, p0=p0,
+                labels=labels, model=IC_2, prior=lnprior, nwalkers=10, nburn=0,
+                prefit=True)
 
-    def time_ECPL_memoize(self):
-        self.ecpl._memoize = True
-        for i in range(10):
-            pd = self.ecpl(self.energy_g)
-
-    def time_Sy(self):
-        Sy = naima.models.Synchrotron(self.pdist, B=1*u.mG)
-        sed = Sy.sed(self.energy_x)
-
-    def time_IC(self):
-        IC = naima.models.InverseCompton(self.pdist,
-                seed_photon_fields=['CMB','FIR'])
-        sed = IC.sed(self.energy_g)
-
-    def time_ICx5(self):
-        IC = naima.models.InverseCompton(self.pdist,
-                seed_photon_fields=['CMB','FIR'])
-        for i in range(5):
-            sed = IC.sed(self.energy_g)
-
-    def time_ICx5_nomemoize(self):
-        IC = naima.models.InverseCompton(self.pdist,
-                seed_photon_fields=['CMB','FIR'])
-        IC._memoize = False
-        for i in range(5):
-            sed = IC.sed(self.energy_g)
-
-    def time_PionDecay(self):
-        PP = naima.models.PionDecay(self.pdist)
-        sed = PP.sed(self.energy_g)
