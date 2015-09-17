3a225d4155e72e40d75cbdc36b08ce1cf81a7754
Víctor Zabalza <vzabalza@gmail.com>
2015-09-17 11:59:31 +0100

    redirect cron output

--- cron.sh
+++ cron.sh
@@ -15,8 +15,8 @@ export PATH=/home/vzabalza/anaconda/bin:$PATH
 cd /home/vzabalza/src/naima-benchmarks
 
 asv run NEW
-git add results/draco
-git commit -am 'new results'
+git add results
+git commit -m 'new results'
 git push
 asv gh-pages
 
